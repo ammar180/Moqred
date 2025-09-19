bf14ea86ce97bc7494c98d526f333e6de9abe4fe
ammar180 <ammaromran.work@gmail.com>
2025-09-19 11:23:15 +0300

    fix: env variables & add pb_schema.json

--- lib/utils/secure_config.dart
+++ lib/utils/secure_config.dart
@@ -1,7 +1,7 @@
 class SecureConfig {
   // TODO: Replace with your Pocketbase credentials
-  static const String pocketBaseUrl = String.fromEnvironment('PB_URL', defaultValue: 'REPLACE_ME');
-  static const String pocketBaseAdminEmail = String.fromEnvironment('PB_ADMIN_EMAIL', defaultValue: 'https://example.com');
-  static const String pocketBaseAdminPassword = String.fromEnvironment('PB_ADMIN_PASSWORD', defaultValue: 'admin@example.com');
+  static const String pocketBaseUrl = String.fromEnvironment('PB_URL', defaultValue: 'https://example.com');
+  static const String pocketBaseAdminEmail = String.fromEnvironment('PB_ADMIN_EMAIL', defaultValue: 'admin@example.com');
+  static const String pocketBaseAdminPassword = String.fromEnvironment('PB_ADMIN_PASSWORD', defaultValue: 'REPLACE_ME');
 }
 
